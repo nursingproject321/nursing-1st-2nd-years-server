a67324e9f8b97d509255b79bc187f78311a9639a
nursingproject321 <145587965+nursingproject321@users.noreply.github.com>
2024-03-18 10:42:15 -0400

    bug fix for db connection

--- utils.js
+++ utils.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export async function connectToDB() {
     try {
-        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING);
+        await mongoose.connect(process.env.MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
     } catch (err) {
         throw new Error(`Error while connecting to db - ${err}`);
     }
